44a4a92233e97fd67006cec5e515c0b17a3ef6c1
Miranda Zhao <mirandazhao@Mirandas-MacBook-Pro.local>
2018-06-08 15:29:24 -0500

    initial upload

---
+++ initial_processing.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Usage: 
+#Initial_Processing.sh <Input_filename> <Output_filename>
+#To strip the comma inside of quotes, take only certain columns:
+cat $1 | \
+awk -F'"' -v OFS='' '{ for (i=2; i<=NF; i+=2) gsub(",", "", $i) } 1' | \
+cut -d, -f-16,18-19,21-22,24 | \
+sed s'/\$//g' > $2
