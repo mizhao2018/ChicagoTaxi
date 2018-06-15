d2c85db541c5ed74d509888a0473310e51e0c7e3
Miranda Zhao <mirandazhao@Mirandas-MacBook-Pro.local>
2018-06-15 10:24:20 -0500

    updated with time series

--- CSV Files/initial_processing.sh
+++ CSV Files/initial_processing.sh
@@ -5,5 +5,5 @@
 #To strip the comma inside of quotes, take only certain columns:
 cat $1 | \
 awk -F'"' -v OFS='' '{ for (i=2; i<=NF; i+=2) gsub(",", "", $i) } 1' | \
-cut -d, -f-16,18-19,21-22,24 | \
+cut -d, -f-6,9-16,18-19,21-22 | \
 sed s'/\$//g' > $2
---
+++ CSV Files/my_cp.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+export MYIP=18.218.230.97
+
+scp -i $HOME/.ssh/aws_key.pem xae ubuntu@$MYIP:~/.
+scp -i $HOME/.ssh/aws_key.pem xaf ubuntu@$MYIP:~/.
+scp -i $HOME/.ssh/aws_key.pem xag ubuntu@$MYIP:~/.
+scp -i $HOME/.ssh/aws_key.pem xah ubuntu@$MYIP:~/.
+scp -i $HOME/.ssh/aws_key.pem xai ubuntu@$MYIP:~/.
+scp -i $HOME/.ssh/aws_key.pem xaj ubuntu@$MYIP:~/.
